ed2af590d99fabd8ce9008ccceb64c6e5c2828f5
LONGDHSE63585\Administrator <longdo00311@gmail.com>
2018-10-01 15:49:15 +0700

    account func

---
+++ Server/DataService/DataService/Models/Entities/Services/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using DataService.Models.Entities.Repositories;
+using DataService.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Models.Entities.Services
+{
+    public partial interface IAccountService
+    {
+        bool CheckLogin(string username, string password,int roleid);
+    }
+
+    public partial class AccountService
+    {
+        public bool CheckLogin(string username, string password, int roleid)
+        {            
+            var accountRepo = DependencyUtils.Resolve<IAccountRepository>();
+            var isLoginSucess = accountRepo.GetActive().Any(a => a.Username == username && a.Passwrod == password && a.RoleId == 1);
+            return isLoginSucess;
+        }
+    }
+}
