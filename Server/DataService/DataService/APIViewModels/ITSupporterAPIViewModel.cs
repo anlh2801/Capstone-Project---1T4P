7ac075cb87e67b808ddf122c378a5d68788b6615
CHIENNTSE62036\ChienNT <thanhchien12596@gmail.com>
2018-10-06 22:09:33 +0700

    commit ODTS49

---
+++ Server/DataService/DataService/APIViewModels/ITSupporterAPIViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.APIViewModels
+{
+    public class ITSupporterAPIViewModel : DataService.ViewModels.BaseEntityViewModel<DataService.Models.Entities.ITSupporter>
+    {
+        public ITSupporterAPIViewModel() : base() { }
+        public ITSupporterAPIViewModel(DataService.Models.Entities.ITSupporter entity) : base(entity) { }
+
+        public  int ITSupporterId { get; set; }
+        public  string ITSupporterName { get; set; }
+        public  int AccountId { get; set; }
+        public string Username { get; set; }
+        public  string Telephone { get; set; }
+        public  string Email { get; set; }
+        public  string Gender { get; set; }
+        public  string Address { get; set; }
+        public Nullable<double> RatingAVG { get; set; }
+        public  string IsBusy { get; set; }
+        public  bool IsDelete { get; set; }
+        public  string CreateDate { get; set; }
+        public  string UpdateDate { get; set; }
+    }
+}
---
+++ Server/DataService/DataService/Models/Entities/Services/ITSupporterService.cs
@@ -0,0 +1,47 @@
+﻿using DataService.APIViewModels;
+using DataService.Models.Entities.Repositories;
+using DataService.Utilities;
+using DataService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Models.Entities.Services
+{
+    public partial interface IITSupporterService
+    {
+        List<ITSupporterAPIViewModel> GetAllITSupporter();
+    }
+
+    public partial class ITSupporterService
+    {
+        public List<ITSupporterAPIViewModel> GetAllITSupporter()
+        {
+            List<ITSupporterAPIViewModel> rsList = new List<ITSupporterAPIViewModel>();
+            var ITSupporterRepo = DependencyUtils.Resolve<IITSupporterRepository>();
+            var companies = ITSupporterRepo.GetActive().ToList();
+
+            foreach (var item in companies)
+            {                
+                rsList.Add(new ITSupporterAPIViewModel
+                {
+                    ITSupporterId = item.ITSupporterId,
+                    ITSupporterName = item.ITSupporterName,
+                    Username = item.Account.Username,
+                    Telephone = item.Telephone,
+                    Email = item.Email,
+                    Gender = item.Gender,
+                    Address = item.Address,
+                    RatingAVG = item.RatingAVG,
+                    IsBusy = item.IsBusy,
+                    CreateDate = item.CreateDate != null ? item.CreateDate.Value.ToString("MM/dd/yyyy") : string.Empty,
+                    UpdateDate = item.UpdateDate != null ? item.UpdateDate.Value.ToString("MM/dd/yyyy") : string.Empty
+                });
+            }
+
+            return rsList;
+        }
+}
+}
