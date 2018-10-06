55e411c97d105ffdd0b37f0c5a2e8df300c408e2
CHIENNTSE62036\ChienNT <thanhchien12596@gmail.com>
2018-10-06 18:45:02 +0700

    move API => Controller

---
+++ Server/CapstoneProject_ODTS/CapstoneProject_ODTS.MVC/Controllers/ContractController.cs
@@ -0,0 +1,35 @@
+﻿using DataService.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneProject_ODTS.Controllers
+{
+    public class ContractController : Controller
+    {
+        private ContractDomain _contractDomain;
+
+        public ContractController()
+        {
+            _contractDomain = new ContractDomain();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        public ActionResult GetAllContract()
+        {
+            var contracts = _contractDomain.GetAllContract();
+            if (contracts.Count() < 0)
+            {
+                return Json(new { result = "" }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = contracts }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Server/CapstoneProject_ODTS/CapstoneProject_ODTS.MVC/Controllers/ProblemController.cs
@@ -0,0 +1,71 @@
+﻿using DataService.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneProject_ODTS.Controllers
+{
+    public class ProblemController : Controller
+    {
+        private ProblemDomain _problemDomain;
+
+        public ProblemController()
+        {
+            _problemDomain = new ProblemDomain();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        public ActionResult GetAllTicket()
+        {
+            var result = _problemDomain.GetAllProblem();
+            if (result.Count() < 0)
+            {
+                //không co record
+            }
+            
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult TicketDetail(Int32 id)
+        {
+            ViewBag.Title = "Home Page";
+            ViewData["ID"] = id.ToString();
+            return View();
+        }
+
+        public ActionResult GetTicketDetail(int problemId)
+        {
+            var result = _problemDomain.GetTicketByProblemId(problemId);
+            if (result.Count() < 0)
+            {
+                //không co record
+            }
+
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult TicketStatus(Int32 id)
+        {
+            ViewBag.Title = "Home Page";
+            ViewData["STATUS"] = id.ToString();
+            return View();
+        }
+
+        public ActionResult GetTicketWithStatus(int status)
+        {
+            var result = _problemDomain.GetProblemWithStatus(status);
+            if (result.Count() < 0)
+            {
+                //không co record
+            }
+
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
