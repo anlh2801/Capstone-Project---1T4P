95045e0937ce77fcd409069407f9a7831d42b03c
CHIENNTSE62036\ChienNT <thanhchien12596@gmail.com>
2018-10-06 23:36:31 +0700

    Commit

---
+++ Server/DataService/DataService/APIViewModels/TicketAPIViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.APIViewModels
+{
+    public class TicketAPIViewModel : DataService.ViewModels.BaseEntityViewModel<DataService.Models.Entities.Ticket>
+    {
+        public TicketAPIViewModel() : base() { }
+        public TicketAPIViewModel(DataService.Models.Entities.Ticket entity) : base(entity) { }
+
+        public int TicketId { get; set; }
+        public int ServiceItemId { get; set; }
+        public int RequestId { get; set; }
+        public int DeviceId { get; set; }
+        public string Desciption { get; set; }
+        public int Current_TicketStatus { get; set; }
+        public int CurrentITSupporter_Id { get; set; }
+        public int Rating { get; set; }
+        public double Estimation { get; set; }
+        public string StartTime { get; set; }
+        public string Endtime { get; set; }
+        public string CreateDate { get; set; }
+        public string UpdateDate { get; set; }
+    }
+}
---
+++ Server/DataService/DataService/Domain/ITSupporterDomain.cs
@@ -0,0 +1,53 @@
+﻿using DataService.APIViewModels;
+using DataService.Models.Entities.Services;
+using DataService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Domain
+{
+    public interface IITSupporterDomain
+    {
+
+        bool UpdateTicketStatus(ITSupporterUpdateAPIViewModel model);
+
+        List<ITSupporterAPIViewModel> ViewProfileITSupporter(int ITsupporter_id);
+
+        List<TicketAPIViewModel> ViewAllOwnerTicket(int ITsupporter_id);
+
+    }
+
+    public class ITSupporterDomain : BaseDomain, IITSupporterDomain
+    {
+
+        public bool UpdateTicketStatus(ITSupporterUpdateAPIViewModel model)
+        {
+            var iTSupporterService = this.Service<IITSupporterService>();
+
+            var result = iTSupporterService.UpdateTicketStatus(model);
+
+            return result;
+        }
+
+        public List<ITSupporterAPIViewModel> ViewProfileITSupporter(int ITsupporter_id)
+        {
+            var ITSupporterService = this.Service<IITSupporterService>();
+
+            var ITSupporter = ITSupporterService.ViewProfileITSupporter(ITsupporter_id);
+
+            return ITSupporter;
+        }
+
+        public List<TicketAPIViewModel> ViewAllOwnerTicket(int ITsupporter_id)
+        {
+            var ITSupporterService = this.Service<IITSupporterService>();
+
+            var ITSupporter = ITSupporterService.ViewAllOwnerTicket(ITsupporter_id);
+
+            return ITSupporter;
+        }
+    }
+}
