d4a86b4557b6ee451b8e40d8c9e3b0c44aa2f690
tienpp5695 <dark5695@gmail.com>
2018-10-06 14:11:54 +0700

    validate null for Date

--- Server/DataService/DataService/Models/Entities/Services/DeviceService.cs
+++ Server/DataService/DataService/Models/Entities/Services/DeviceService.cs
@@ -33,16 +33,16 @@ namespace DataService.Models.Entities.Services
                         DeviceTypeId = item.DeviceTypeId,
                         DeviceName = item.DeviceName,
                         DeviceCode = item.DeviceCode,
-                        GuarantyStartDate = item.GuarantyStartDate.Value.ToString("MM/dd/yyyy"),
-                        GuarantyEndDate = item.GuarantyEndDate.Value.ToString("MM/dd/yyyy"),
+                        GuarantyStartDate = item.GuarantyStartDate != null ? item.GuarantyStartDate.Value.ToString("MM/dd/yyyy") : string.Empty,
+                        GuarantyEndDate = item.GuarantyEndDate != null ? item.GuarantyEndDate.Value.ToString("MM/dd/yyyy") : string.Empty,
                         Ip = item.Ip,
                         Port = item.Port,
                         DeviceAccount = item.DeviceAccount,
                         DevicePassword = item.DevicePassword,
-                        SettingDate = item.SettingDate.Value.ToString("MM/dd/yyyy"),
+                        SettingDate = item.SettingDate != null ? item.SettingDate.Value.ToString("MM/dd/yyyy") : string.Empty,
                         Other = item.Other,
-                        CreateDate = item.CreateDate.Value.ToString("MM/dd/yyyy"),
-                        UpdateDate = item.UpdateDate.Value.ToString("MM/dd/yyyy")
+                        CreateDate = item.CreateDate != null ? item.CreateDate.Value.ToString("MM/dd/yyyy") : string.Empty,
+                        UpdateDate = item.UpdateDate != null ? item.UpdateDate.Value.ToString("MM/dd/yyyy") : string.Empty
                     });
                 }
 
