2083d0f712108bc91566b51b39bb250b27983002
tienpp5695 <dark5695@gmail.com>
2018-10-09 22:13:36 +0700

    ODTS-40: API_ITSupporter:Set Priority Task ODTS-41: API_ITSupporte: Monitor Time Task ODTS-44: API_ITSupporte: View GuideLine

---
+++ Server/DataService/DataService/APIViewModels/ITSupporterSetPriorityTaskAPIViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.APIViewModels
+{
+    public class ITSupporterSetPriorityTaskAPIViewModel : DataService.ViewModels.BaseEntityViewModel<DataService.Models.Entities.TicketTask>
+    {
+        public ITSupporterSetPriorityTaskAPIViewModel() : base() { }
+        public ITSupporterSetPriorityTaskAPIViewModel(DataService.Models.Entities.TicketTask entity) : base(entity) { }
+
+        public int TicketTaskId { get; set; }
+        public int Priority { get; set; }
+    }
+}
