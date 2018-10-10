b26d515d9fa0e0965ad05289089fcd7658a5f221
LONGDHSE63585\Administrator <longdo00311@gmail.com>
2018-10-10 16:33:35 +0700

    add

---
+++ Server/DataService/DataService/APIViewModels/AccountCreateAPIViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DataService.Models.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.APIViewModels
+{
+    public class AccountCreateAPIViewModel : DataService.ViewModels.BaseEntityViewModel<DataService.Models.Entities.Account>
+    {
+        public AccountCreateAPIViewModel() : base() { }
+        public AccountCreateAPIViewModel(DataService.Models.Entities.Account entity) : base(entity) { }
+
+        public int NumericalOrder { get; set; }
+        public int AccountId { get; set; }
+        public int RoleId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsDelete { get; set; }
+        public string CreateAt { get; set; }
+        public string UpdateAt { get; set; }
+    }
+}
---
+++ Server/DataService/DataService/Domain/RoleDomain.cs
@@ -0,0 +1,27 @@
+﻿using DataService.APIViewModels;
+using DataService.Models.Entities.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Domain
+{
+
+    public interface IRoleDomain
+    {
+        List<RoleAPIViewModel> GetAllRole();
+    };
+    public class RoleDomain : BaseDomain, IRoleDomain
+    {
+        public List<RoleAPIViewModel> GetAllRole()
+        {
+            var roleService = this.Service<IRoleService>();
+
+            var roles = roleService.GetAllRole();
+
+            return roles;
+        }
+    }
+}
