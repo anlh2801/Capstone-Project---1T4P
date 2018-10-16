fa4d3377e52ff90d9338d3bc77b33a51ad67c8f8
LONGDHSE63585\Administrator <longdo00311@gmail.com>
2018-10-16 19:19:26 +0700

    Test

--- Server/CapstoneProject_ODTS/CapstoneProject_ODTS.MVC/Controllers/DeviceTypeController.cs
+++ Server/CapstoneProject_ODTS/CapstoneProject_ODTS.MVC/Controllers/DeviceTypeController.cs
@@ -28,43 +28,5 @@ namespace CapstoneProject_ODTS.Controllers
 
             return Json(new { result = devicetypes }, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult ViewDetail(int devicetype_id)
-        {
-            var devicetypedetail = _devicetypeDomain.ViewDetail(devicetype_id);
-            if (devicetypedetail == null)
-            {
-                return Json(new { result = "" }, JsonRequestBehavior.AllowGet);
-            }
-
-            return Json(new { result = devicetypedetail }, JsonRequestBehavior.AllowGet);
-        }
-        public ActionResult CreateDeviceType(DeviceTypeAPIViewModel model)
-        {
-
-            var result = _devicetypeDomain.CreateDeviceType(model);
-            if (result == false)
-            {
-                return Json(false); ;
-            }
-
-            return Json(true);
-
-        }
-        public ActionResult UpdateDeviceType(DeviceTypeAPIViewModel model)
-        {
-            var result = _devicetypeDomain.UpdateDeviceType(model);
-            if (result == false)
-            {
-                return Json(new { result = "" }, JsonRequestBehavior.AllowGet);
-            }
-
-            return Json(new { result }, JsonRequestBehavior.AllowGet);
-
-        }
-        public ActionResult RemoveDeviceType(int devicetype_id)
-        {
-            var devicetypeDetail = _devicetypeDomain.RemoveDeviceType(devicetype_id);
-            return Json(new { result = devicetypeDetail }, JsonRequestBehavior.AllowGet);
-        }
     }
 }
