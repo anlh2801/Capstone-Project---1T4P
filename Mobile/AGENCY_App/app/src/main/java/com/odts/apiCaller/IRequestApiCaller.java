1bebe5120d6bcb724254b51ed620a335b72b12cd
CHIENNTSE62036\ChienNT <thanhchien12596@gmail.com>
2018-10-21 11:57:05 +0700

    tab with fragment

--- Mobile/AGENCY_App/app/src/main/java/com/odts/apiCaller/IRequestApiCaller.java
+++ Mobile/AGENCY_App/app/src/main/java/com/odts/apiCaller/IRequestApiCaller.java
@@ -2,12 +2,16 @@ package com.odts.apiCaller;
 
 import com.odts.models.Request;
 import com.odts.utils.ResponseObject;
+import com.odts.utils.ResponseObjectReturnList;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface IRequestApiCaller {
     @POST("agency/create_request")
     Call<ResponseObject<Boolean>> addUser(@Body Request request);
+    @GET("agency/get_ticket_by_requestId")
+    Call<ResponseObjectReturnList<Request> >
 }
