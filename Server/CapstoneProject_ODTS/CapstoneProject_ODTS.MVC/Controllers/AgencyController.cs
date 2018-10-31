9a22a9d4d1b746305a23047f7e7f0925596a4168
LONGDHSE63585\Administrator <longdo00311@gmail.com>
2018-10-31 13:06:08 +0700

    MVC chuyển hướng

--- Server/CapstoneProject_ODTS/CapstoneProject_ODTS.MVC/Controllers/AgencyController.cs
+++ Server/CapstoneProject_ODTS/CapstoneProject_ODTS.MVC/Controllers/AgencyController.cs
@@ -39,7 +39,11 @@ namespace CapstoneProject_ODTS.Controllers
             ViewData["ID"] = id.ToString();
             return View();
         }
-
+        public ActionResult CompanyAgency(int id)
+        {
+            ViewData["ID"] = id.ToString();
+            return View();
+        }
 
         public ActionResult GetAllDevice(int agency_id)
         {
