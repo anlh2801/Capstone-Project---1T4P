aae10814c92490a6734ea0ce577be6c5e4884a52
khainv <khai231196@gmail.com>
2018-11-07 23:27:45 +0700

    add_request_layout

---
+++ Mobile/AGENCY_App/app/src/main/java/com/odts/activities/AddRequestLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.odts.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRequestLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_request_layout);
+    }
+}
