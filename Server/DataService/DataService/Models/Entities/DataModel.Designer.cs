fc13e074eabb0f3decc5f4371f152e91ae6d620d
ANLHSE61808\ANLHSE61808 <spa2801@gmail.com>
2018-11-10 17:42:12 +0700

    update UI

--- Server/DataService/DataService/Models/Entities/DataModel.Designer.cs
+++ Server/DataService/DataService/Models/Entities/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\FilesOfTien\capstone\Capstone-Project\Server\DataService\DataService\Models\Entities\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\DevOJT\Capstone Project - 1T4P\Server\DataService\DataService\Models\Entities\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
