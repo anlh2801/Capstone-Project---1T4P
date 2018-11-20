a96f3d9fef4ba4d72f81e871173cfd388e03a707
CHIENNTSE62036\ChienNT <thanhchien12596@gmail.com>
2018-11-20 22:40:03 +0700

    fix Process Fragment - ChienNT

--- Mobile/AGENCY_App/app/src/main/java/com/odts/activities/ProcessingFragment.java
+++ Mobile/AGENCY_App/app/src/main/java/com/odts/activities/ProcessingFragment.java
@@ -10,8 +10,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
 import com.odts.customTools.ProcessRequestAdapter;
 import com.odts.models.Request;
+import com.odts.models.RequestGroupMonth;
 import com.odts.services.RequestService;
 import com.odts.utils.CallBackData;
 import com.odts.utils.Enums;
@@ -22,7 +25,7 @@ import java.util.ArrayList;
 public class ProcessingFragment extends Fragment {
 
     private RequestService requestService;
-    private RecyclerView recyclerView;
+    private ListView listView;
     private ProcessRequestAdapter pendingRequestAdapter;
     Integer agencyId = 0;
 
@@ -42,16 +45,16 @@ public class ProcessingFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_processing, container, false);
         SharedPreferences share = getActivity().getApplicationContext().getSharedPreferences("ODTS", 0);
         SharedPreferences.Editor edit = share.edit();
-        recyclerView = (RecyclerView) v.findViewById(R.id.listpro);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        listView =v.findViewById(R.id.listProcess);
         agencyId = share.getInt("agencyId", 0);
 
         requestService = new RequestService();
-        requestService.getRequestByStatus(getActivity(), agencyId, Enums.RequestStatusEnum.Processing.getIntValue(), new CallBackData<ArrayList<Request>>() {
+        requestService.getRequestByStatusWithMonth(getActivity(), agencyId, Enums.RequestStatusEnum.Processing.getIntValue(), new CallBackData<ArrayList<RequestGroupMonth>>() {
             @Override
-            public void onSuccess(ArrayList<Request> listRequests) {
-                pendingRequestAdapter = new ProcessRequestAdapter(getActivity(), listRequests);
-                recyclerView.setAdapter(pendingRequestAdapter);
+            public void onSuccess(ArrayList<RequestGroupMonth> listRequests) {
+                pendingRequestAdapter = new ProcessRequestAdapter(getActivity(), R.layout.list_processing_request_group_item, listRequests);
+                listView.setAdapter(pendingRequestAdapter);
+//                recyclerView.getItemAtPosition(po)
             }
 
             @Override
