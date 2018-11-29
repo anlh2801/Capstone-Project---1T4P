da559baecda24ecdb7ac020c46934f26739aed62
CHIENNTSE62036\ChienNT <thanhchien12596@gmail.com>
2018-11-29 14:48:48 +0700

    DoFragment - ChienNT

--- Mobile/IT_SUPPORTER_App/app/src/main/java/com/odts/it_supporter_app/activities/AgencyFragment.java
+++ Mobile/IT_SUPPORTER_App/app/src/main/java/com/odts/it_supporter_app/activities/AgencyFragment.java
@@ -16,17 +16,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.odts.it_supporter_app.R;
+import com.odts.it_supporter_app.apiCaller.DeviceAdapter;
+import com.odts.it_supporter_app.models.Device;
+import com.odts.it_supporter_app.models.DeviceHistory_Ticket;
 import com.odts.it_supporter_app.models.Request;
+import com.odts.it_supporter_app.models.Ticket;
 import com.odts.it_supporter_app.services.ITSupporterService;
 import com.odts.it_supporter_app.services.RequestService;
 import com.odts.it_supporter_app.utils.CallBackData;
 
-/**
+import java.util.ArrayList;
+import java.util.List;
 
+/**
  * create an instance of this fragment.
  */
 public class AgencyFragment extends android.support.v4.app.Fragment {
@@ -35,7 +43,6 @@ public class AgencyFragment extends android.support.v4.app.Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-
      * @return A new instance of fragment AgencyFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -44,7 +51,10 @@ public class AgencyFragment extends android.support.v4.app.Fragment {
     String serviceItemName;
     Integer serviceItemId = 0;
     RequestService requestService;
+    LinearLayout historyAgency, historyDevice;
+    DeviceAdapter deviceAdapter;
     com.getbase.floatingactionbutton.FloatingActionButton btnCall, btnChat;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,10 +73,13 @@ public class AgencyFragment extends android.support.v4.app.Fragment {
         final TextView createDate = v.findViewById(R.id.CreateDateAgencyDetail);
         final TextView txtAddressAgency = v.findViewById(R.id.AgencyAdressAgencuDetails);
         final TextView txtPhoneAgency = v.findViewById(R.id.AgencyPhoneAgencuDetails);
+        final TextView numberDevice = v.findViewById(R.id.numberDevice);
+//        final ImageButton deviceDetail = v.findViewById(R.id.imageButton4);
         btnCall = v.findViewById(R.id.action_a);
         btnChat = v.findViewById(R.id.action_b);
         final FloatingActionsMenu menu = v.findViewById(R.id.multiple_actions);
-
+        historyAgency = v.findViewById(R.id.HistoryAgency);
+        historyDevice = v.findViewById(R.id.historyDevice);
         menu.bringToFront();
 
         //ImageButton scan = v.findViewById(R.id.imageButton3);
@@ -88,12 +101,14 @@ public class AgencyFragment extends android.support.v4.app.Fragment {
         requestService.getRequestByRequestIdAndITSupporterId(getActivity(), itSupporterId, new CallBackData<Request>() {
             @Override
             public void onSuccess(final Request request) {
+                int numberDeviceString = request.getTicket().size();
+                numberDevice.setText(String.valueOf(numberDeviceString));
                 agencyName.setText(request.getAgencyName());
                 //rqName.setText(request.getRequestName());
-                if (request.getPriority().equalsIgnoreCase("Xử lý gấp")){
+                if (request.getPriority().equalsIgnoreCase("Xử lý gấp")) {
                     priority.setText(request.getPriority());
                     priority.setTextColor(Color.parseColor("#C62828"));
-                } else if (request.getPriority().equalsIgnoreCase("Cần xử lý")){
+                } else if (request.getPriority().equalsIgnoreCase("Cần xử lý")) {
                     priority.setText(request.getPriority());
                     priority.setTextColor(Color.parseColor("#F9A825"));
                 } else {
@@ -120,7 +135,6 @@ public class AgencyFragment extends android.support.v4.app.Fragment {
                         }
                     }
                 });
-
                 btnChat.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -129,6 +143,65 @@ public class AgencyFragment extends android.support.v4.app.Fragment {
                         startActivity(intent);
                     }
                 });
+
+                List<Device> listDevices = new ArrayList<>();
+
+                for (final Ticket item : request.getTicket()) {
+                    View v = getLayoutInflater().inflate(R.layout.device_item, null);
+                    TextView txtDeviceName = (TextView) v.findViewById(R.id.txtDeviceName);
+                    txtDeviceName.setText(item.getDeviceName());
+                    ImageButton deviceDetail = (ImageButton) v.findViewById(R.id.deviceDetail);
+                    deviceDetail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(getContext(), DeviceInfoActivity.class);
+                            intent.putExtra("deviceCode", item.getDeviceCode());
+                            getContext().startActivity(intent);
+                        }
+                    });
+                    historyDevice.addView(v);
+                }
+                requestService.getRequestHistoryByAgency(getActivity(), request.getAgencyId(), new CallBackData<ArrayList<Request>>() {
+                    @Override
+                    public void onSuccess(ArrayList<Request> requests) {
+
+
+                        for (Request item : requests) {
+                            View v1 = getLayoutInflater().inflate(R.layout.device_history_item, null);
+                            TextView txtHienTuong = (TextView) v1.findViewById(R.id.txtHienTuong);
+                            TextView txtNgayTao = (TextView) v1.findViewById(R.id.txtNgayTao);
+                            txtHienTuong.setText("Hiện tượng: " + item.getRequestName());
+                            txtNgayTao.setText("Đã bị sự cố ngày: " + item.getCreateDate());
+                            historyAgency.addView(v1);
+                        }
+                    }
+
+                    @Override
+                    public void onFail(String message) {
+
+                    }
+                });
+
+//                deviceDetail.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                        List<Device> listDevices = new ArrayList<>();
+//                        View v = getLayoutInflater().inflate(R.layout.agency_detail, null);
+//                        ListView listViewDeviceC = (ListView) v.findViewById(R.id.listDevice);
+//                        for (Ticket item : request.getTicket()) {
+//
+//                            Device device = new Device();
+//                            device.setDeviceId(item.getDeviceId());
+//                            device.setDeviceName(item.getDeviceName());
+//                            device.setDeviceCode(item.getDeviceCode());
+//                            listDevices.add(device);
+//                        }
+//                        deviceAdapter = new DeviceAdapter(getActivity(), R.layout.device_item, listDevices);
+//                        listViewDeviceC.setAdapter(deviceAdapter);
+//                    }
+//                });
+
             }
 
             @Override
